d1f2b00fdba9ea404b3cd6261b81dda4d13fd7e8
Reka Kulinyi <reekaa85@gmail.com>
2020-08-03 13:31:20 +0200

    creating Role class

---
+++ spring-mvc-online-school/src/main/java/com/onlineschool/demo/entity/Role.java
@@ -0,0 +1,54 @@
+package com.onlineschool.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+
+	// - - - constructors - - -
+	public Role() {
+	}
+
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+
+	// - - - getters and setters - - -
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// - - - toString() - - -
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + "]";
+	}
+	
+}
