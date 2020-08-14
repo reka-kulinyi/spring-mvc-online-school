a09f98e08e3381c0ef3d3a73e806b4aae7444790
Reka Kulinyi <reekaa85@gmail.com>
2020-08-14 18:02:43 +0200

    registration controller

---
+++ spring-mvc-online-school/src/main/java/com/onlineschool/demo/controller/RegistrationController.java
@@ -0,0 +1,69 @@
+package com.onlineschool.demo.controller;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.onlineschool.demo.entity.User;
+import com.onlineschool.demo.service.UserService;
+import com.onlineschool.demo.user.SchoolUser;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+	@Autowired
+	private UserService userService;
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
+	@GetMapping("/showRegistrationForm")
+	public String showLoginPage(Model model) {
+		model.addAttribute("schoolUser", new SchoolUser());
+		return "registration-page";
+	}
+	
+	@PostMappping("/processRegistrationForm")
+	public String processRegistrationForm(
+			@Valid @ModelAttribute("schoolUser") SchoolUser schooluser,
+			BindingResult bindingResult,
+			Model model
+			) {
+		String username = schooluser.getUsername();
+		logger.info("Processing registration form for: " + username);
+		
+		// form validation
+		if(bindingResult.hasErrors()) {
+			return "registration-page";
+		}
+		
+		// check the database if user already exists
+		User existing = userService.findByUsername(username);
+		if(existing != null) {
+			model.addAttribute("schoolUser", new SchoolUser());
+			model.addAttribute("registrationError", "Username already exists")
+			
+			logger.warning("Username already exists");
+			return "registration-page";
+		}
+		
+		// create new user account
+		userService.save(schooluser);
+		logger.info("Successfully created user: " + username);
+		return "registration-confirmation";
+	}
+}
---
+++ spring-mvc-online-school/src/main/webapp/WEB-INF/view/registration-confirmation.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Registration Confirmation</title>
+	</head>
+
+	<body>
+	
+		<h2>User registered successfully!</h2>
+	
+		<hr>
+		
+		<a href="${pageContext.request.contextPath}/loginPage">Login with new user</a>
+		
+	</body>
+
+</html>
