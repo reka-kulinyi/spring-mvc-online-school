98820cf1484e2022c4aaf7dae445bf43479b7054
Reka Kulinyi <reekaa85@gmail.com>
2020-08-14 16:37:32 +0200

    loadUserByUsername method added

--- spring-mvc-online-school/src/main/java/com/onlineschool/demo/config/SecurityWebApplicationInitializer.java
+++ spring-mvc-online-school/src/main/java/com/onlineschool/demo/config/SecurityWebApplicationInitializer.java
@@ -1,5 +1,7 @@
 package com.onlineschool.demo.config;
 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
 public class SecurityWebApplicationInitializer 
 extends AbstractSecurityWebApplicationInitializer {
 
--- spring-mvc-online-school/src/main/java/com/onlineschool/demo/service/UserServiceImpl.java
+++ spring-mvc-online-school/src/main/java/com/onlineschool/demo/service/UserServiceImpl.java
@@ -1,20 +1,35 @@
 package com.onlineschool.demo.service;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.onlineschool.demo.dao.RoleDao;
 import com.onlineschool.demo.dao.UserDao;
 import com.onlineschool.demo.entity.User;
+import com.onlineschool.demo.entity.Role;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserDao userDao;
+	
+	@Autowired
+	private RoleDao roleDao;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Override
 	@Transactional
@@ -53,7 +68,21 @@ public class UserServiceImpl implements UserService {
 	public User findByUsername(String username) {
 		return userDao.findUserByUsername(username);
 	}
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userDao.findUserByUsername(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("Invalid username or password");
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), 
+				user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+	}
 	
-	
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+	}
 	
 }
