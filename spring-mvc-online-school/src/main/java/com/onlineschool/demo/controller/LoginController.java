72f5f59c8d9bc700196fd39c3cf106b467c130dd
Reka Kulinyi <reekaa85@gmail.com>
2020-08-17 14:58:23 +0200

    creating login controller

---
+++ spring-mvc-online-school/src/main/java/com/onlineschool/demo/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.onlineschool.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/loginPage")
+	public String showLoginPage() {
+		return "login-page";
+	}
+	
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		return "access-denied";
+	}
+}
