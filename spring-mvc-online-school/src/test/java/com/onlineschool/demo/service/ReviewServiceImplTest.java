2013ac3ce732689ceed05a2a756b71f54226bbef
Reka Kulinyi <reekaa85@gmail.com>
2021-01-19 09:24:36 +0100

    tests for ReviewService class

--- spring-mvc-online-school/src/test/java/com/onlineschool/demo/service/ReviewServiceImplTest.java
+++ spring-mvc-online-school/src/test/java/com/onlineschool/demo/service/ReviewServiceImplTest.java
@@ -1,11 +1,21 @@
 package com.onlineschool.demo.service;
 
 import com.onlineschool.demo.dao.ReviewDao;
+import com.onlineschool.demo.entity.Review;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ReviewServiceImplTest {
@@ -14,13 +24,38 @@ class ReviewServiceImplTest {
     private ReviewDao reviewDao;
 
     @InjectMocks
-    CourseService courseService;
+    ReviewService reviewService;
 
     @Test
     void getRecentReviews() {
+        // given
+        List<Review> recentReviews = new ArrayList<>();
+        recentReviews.add(new Review());
+        recentReviews.add(new Review());
+        when(reviewDao.getRecentReviews(anyInt())).thenReturn(recentReviews);
+
+        // when
+        List<Review> returnedReviews = reviewService.getRecentReviews(2);
+
+        //then
+        then(reviewDao).should().getRecentReviews(2);
+        assertThat(returnedReviews).hasSize(2);
     }
 
     @Test
     void getReviewsByInstructor() {
+
+        // given
+        List<Review> reviewsByInstructor = new ArrayList<>();
+        reviewsByInstructor.add(new Review());
+        reviewsByInstructor.add(new Review());
+        when(reviewDao.getReviewsByInstructor(anyLong())).thenReturn(reviewsByInstructor);
+
+        // when
+        List<Review> returnedReviews = reviewService.getReviewsByInstructor(1L);
+
+        //then
+        then(reviewDao).should().getReviewsByInstructor(1L);
+        assertThat(returnedReviews).hasSize(2);
     }
 }
